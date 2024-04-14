3fb03287904fb7fad774bad298da81717e75f684
Yatendra Kumar <97733654+Yatendrakumar627@users.noreply.github.com>
2024-04-14 16:51:00 +0530

    database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-exports.dbConnect = function()
-{
-    mongoose.connect(process.env.MONGODB_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
+exports.dbConnect = function () {
+  const MONGODB_URL =
+    "mongodb+srv://kumaryatendra4500:blog@blog.xfunvt1.mongodb.net/?retryWrites=true&w=majority&appName=Blog";
+
+  mongoose
+    .connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-    .then(console.log('DB Connected Successfully'))
+    .then(console.log("DB Connected Successfully"))
     .catch((error) => {
-        console.log('DB Connection Issue');
-        console.error(error);
-        process.exit(1);
-    })
-}
+      console.log("DB Connection Issue");
+      console.error(error);
+      process.exit(1);
+    });
+};
+
+
